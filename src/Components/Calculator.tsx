97f14e01b1b0930d77babf690b2f0fc98b3d8de0
Spencer Whitehead <53270623+spwhitehead@users.noreply.github.com>
2024-07-16 12:01:51 -0600

    Handle name input

--- src/Components/Calculator.tsx
+++ src/Components/Calculator.tsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Calculator: React.FC = () => {
+    const [name, setName] = useState("");
+    const [ageInYears, setAgeInYears] = useState();
+
+    const handleAgeChange = (event: React.ChangeEvent<HTMLInputElement>): void => setAgeInYears(event.target.value);
+    
     return (
-        <></>
+        <>
+        <form className="calculatorForm">
+            <div>
+                <label htmlFor="...">Name</label>
+                <input
+                    type="text"
+                    id="name"
+                    value={name}
+                    placeholder="Sebastian Olivera"
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>): void => setName(event.target.value)}/>
+
+                <label htmlFor="...">Age</label>
+                <input
+                    type="number"
+                    id="ageInYears"
+                    value={ageInYears}
+                    placeholder="33"
+                    onChange={(handleAgeChange)} />
+            </div>
+
+        </form>
+        </>
     );
 }
 
