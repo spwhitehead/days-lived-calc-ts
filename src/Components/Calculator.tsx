7fa81b62f8ea2d9d886509651fc40fb325b763ad
Spencer Whitehead <53270623+spwhitehead@users.noreply.github.com>
2024-07-17 12:02:18 -0600

    Basic working state

--- src/Components/Calculator.tsx
+++ src/Components/Calculator.tsx
@@ -2,15 +2,28 @@ import React, { useState } from "react";
 
 const Calculator: React.FC = () => {
     const [name, setName] = useState("");
-    const [ageInYears, setAgeInYears] = useState();
+    const [ageInYears, setAgeInYears] = useState<number | null>(null);
+    const [submittedName, setSubmittedName] = useState<string | null>(null);
+    const[submittedAge, setSubmittedAge] = useState<number | null>(null);
 
-    const handleAgeChange = (event: React.ChangeEvent<HTMLInputElement>): void => setAgeInYears(event.target.value);
+    const handleAgeChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        const value = event.target.valueAsNumber;
+        setAgeInYears(value ? value : null);
+    };
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+        event.preventDefault();
+        setSubmittedName(name);
+        setSubmittedAge(ageInYears);
+    };
+
+    const daysLived = submittedAge !== null ? submittedAge * 365 : null;
     
     return (
         <>
-        <form className="calculatorForm">
+        <form className="calculatorForm" action="submit" onSubmit={handleSubmit}>
             <div>
-                <label htmlFor="...">Name</label>
+                <label htmlFor="name">Name</label>
                 <input
                     type="text"
                     id="name"
@@ -18,16 +31,21 @@ const Calculator: React.FC = () => {
                     placeholder="Sebastian Olivera"
                     onChange={(event: React.ChangeEvent<HTMLInputElement>): void => setName(event.target.value)}/>
 
-                <label htmlFor="...">Age</label>
+                <label htmlFor="ageInYears">Age</label>
                 <input
                     type="number"
                     id="ageInYears"
-                    value={ageInYears}
+                    value={ageInYears !== null ? ageInYears : ""}
                     placeholder="33"
                     onChange={(handleAgeChange)} />
             </div>
-
+            <button type="submit">Submit</button>
         </form>
+        {submittedName && submittedAge !== null &&(
+            <div>
+                <p>{submittedName} is {daysLived} days old!</p>
+            </div>
+        )}
         </>
     );
 }
